9d83877dbfef6c033b9cdf63951d7c3e03c79547
uxu-hao <sakira_d@icloud.com>
2025-04-10 19:15:22 +0900

    Initial commit

---
+++ src/app/components/HeroExperience.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { PerspectiveCamera} from "@react-three/drei";
+import { useMemo, useState, useEffect} from "react";
+import HeroModel from "./HeroModel";
+import { Canvas } from "@react-three/fiber";
+
+const BREAKPOINTS = {
+  MOBILE: 768, // モバイル用ブレークポイント
+  TABLET: 1024, // タブレット用ブレークポイント
+} as const;
+
+const Scene = () => {
+  const [screenWidth, setScreenWidth] = useState(0);
+
+  useEffect(() => {
+    // 初期値の設定
+    setScreenWidth(window.innerWidth);
+
+    const handleResize = () => {
+      requestAnimationFrame(() => {
+        setScreenWidth(window.innerWidth);
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const modelProps = useMemo(() => {
+    const viewportSize =
+      screenWidth < BREAKPOINTS.MOBILE
+        ? "mobile"
+        : screenWidth < BREAKPOINTS.TABLET
+        ? "tablet"
+        : "desktop";
+
+    switch (viewportSize) {
+      case "mobile":
+        return {
+          scale: 1,
+          position: [0, -2.5, 0] as [number, number, number],
+        };
+
+      case "tablet":
+        return {
+          scale: 1.2,
+          position: [0, -2, 0] as [number, number, number],
+        };
+
+      case "desktop":
+      default:
+        return {
+          scale: 1.3,
+          position: [0, -2, 0] as [number, number, number],
+        };
+    }
+  }, [screenWidth]);
+
+  return (
+    <>
+      <PerspectiveCamera
+        makeDefault
+        position={[0, 0, 11]}
+      />
+      <HeroModel
+        scale={modelProps.scale}
+        position={modelProps.position}
+        rotation={[0.05, -1.5, 0]}
+      />
+      <ambientLight intensity={1} />
+      <directionalLight
+        position={[10, 10, 10]}
+        intensity={3}
+      />
+    </>
+  );
+};
+
+const HeroExperience = () => {
+  return (
+    <Canvas
+      dpr={[1, 1.5]}
+      performance={{ min: 0.5 }}
+      gl={{
+        powerPreference: "high-performance",
+      }}
+    >
+        <Scene />
+    </Canvas>
+  );
+};
+
+export default HeroExperience;
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import ModelExperience from "./components/HeroExperience";
+export default function Home() {
+  return (
+    <div className="w-full h-screen bg-[111] absolute inset-0">
+      <ModelExperience />
+    </div>
+  );
+}
